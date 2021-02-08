57953f9fcd3796f59afd08ee298ce090ab0b2e5a
Bas van 't Hazeveld <bas@hazeveld.org>
2021-02-08 20:48:37 +0100

    added activity

--- listeners/ready.js
+++ listeners/ready.js
@@ -1,4 +1,5 @@
 const { Listener } = require('discord-akairo');
+const config = require('../config.json');
 
 class ReadyListener extends Listener {
     constructor() {
@@ -10,6 +11,23 @@ class ReadyListener extends Listener {
 
     exec() {
         console.log(`I have logged in as ${this.client.user.username} (${this.client.user.id})`)
+        if(config.user.updated === true){
+            this.client.user.setUsername(config.user.name);
+	        this.client.user.setAvatar(config.user.avatar);
+            console.log("I have updated the username and avatar!")
+        }
+	    if(config.status.enabled){
+		    if(config.status.type === "STREAMING"){
+			    this.client.user.setActivity(config.status.text, {
+				    type: config.status.type,
+				    url: config.status.url
+			    });
+		    }else {
+			    this.client.user.setActivity(config.status.text, {
+				    type: config.status.type
+			    });
+		    }
+	    }
     }
 }
 
