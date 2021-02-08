346eff8a07dc67b5cba4f9c00590e0c1309c0885
Bas van 't Hazeveld <bas@hazeveld.org>
2021-02-08 21:09:16 +0100

    skip command

---
+++ commands/skip.js
@@ -0,0 +1,25 @@
+const { Command } = require('discord-akairo');
+
+class SkipCommand extends Command {
+    constructor(){
+        super('skip', {
+            aliases: ['skip']
+        })
+    }
+
+    exec(message){
+        const serverQueue = this.client.queue.get(call.message.guild.id);
+
+        if(!message.member.voice.channel)// check if user is in voice channel
+            return message.channel.send("You need to join the voice chat first").then(msg => {
+                msg.delete({ timeout: 10000 })
+            })
+        if(!serverQueue)
+            return message.channel.send("There is nothing to skip!").then(msg => {
+                msg.delete({ timeout: 10000 })
+            })
+        serverQueue.connection.dispatcher.end();
+    }
+}
+
+module.exports = SkipCommand;
